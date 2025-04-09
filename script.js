05eba69f85980a9eb2d4d0943882f6ed60bf504f
Bekka590 <Rebekah.bain@phf.uk.com>
2025-04-09 16:15:13 +0100

    Adjustments for mobile viewing

---
+++ script.js
@@ -0,0 +1,7 @@
+// js/scriptBek.js
+const burger = document.getElementById('burger');
+const nav = document.getElementById('nav-links');
+
+burger.addEventListener('click', () => {
+  nav.classList.toggle('active');
+});
